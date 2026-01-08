32b2dd96e10e0618a4bc23fa63c1a61527ec25b3
ManhDevFull <thanhmanhdangfa@gmail.com>
2026-01-08 22:54:50 +0700

    restyle layout

--- src/layout/desktop/HeaderLayout.tsx
+++ src/layout/desktop/HeaderLayout.tsx
@@ -5,7 +5,15 @@ import SearchInput from "@/src/components/searchInput";
 export default function HeaderComponent() {
   return (
     <>
-      <header className="text-white sticky top-0 z-50 h-18 flex items-center px-20 justify-between bg-black">
+      <header className="text-white hidden sticky top-0 z-50 h-18 md:flex items-center px-20 justify-between bg-black">
+        <div className="flex gap-5 items-center">
+          <div>VieC</div>
+          <SearchInput />
+        </div>
+        <MenuHeader />
+        <UserHeader />
+      </header>
+      <header className="text-white flex fixed bottom-0 z-50 h-18 md:hidden items-center px-20 justify-between bg-black">
         <div className="flex gap-5 items-center">
           <div>VieC</div>
           <SearchInput />
