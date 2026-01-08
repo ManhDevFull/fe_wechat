60ba27646d28d1d4c9c4ab00daee150e76cccdc2
ManhDevFull <thanhmanhdangfa@gmail.com>
2026-01-08 16:09:33 +0700

    reset pwa 2

--- src/layout/desktop/HeaderLayout.tsx
+++ src/layout/desktop/HeaderLayout.tsx
@@ -4,7 +4,7 @@ import MenuHeader from "../../components/menuHeader";
 export default function HeaderComponent() {
   return (
     <>
-      <header className="sticky top-0 z-50 h-18 flex items-center px-20 justify-between bg-black">
+      <header className="sticky top-0 z-50 lg:h-18  flex items-center px-20 justify-between bg-black">
         <div id="logo">VieC</div>
         <div id="header-search"></div>
         <MenuHeader />
